44daecacca91cbac4d58aa4e32b72f237c9876ca
Oleg <olezhka.berkut@inbox.ru>
2024-02-26 23:41:20 -0800

    Second

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        ToyStore store = new ToyStore();
+
+        // add some toys
+        store.addToy(1, "Toy 1", 10, 20);
+        store.addToy(2, "Toy 2", 5, 10);
+        store.addToy(3, "Toy 3", 20, 70);
+
+        // set the weight of a toy
+        store.setToyWeight(2, 30);
+
+        // play the game
+        store.play();
+
+        // get the prize toy
+        try {
+            store.getPrizeToy();
+            System.out.println("Игрушка выдана! ");
+        }
+        catch (NullPointerException | IOException e){
+            System.out.println("Игрушки не разыгрывались! ");
+        }
+    }}
